30a073124e13ce3b9f844aa96aa33a6f648524c8
Paul Yevchenko <pavel.yevchenko@gmail.com>
2021-03-16 20:51:26 +0300

    go go

---
+++ BET/Tests/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine(A(null));
+                Console.ReadKey();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static string A([NotNull]string v)
+        {
+            return $"{v}={v}";
+        }
+    }
+}
